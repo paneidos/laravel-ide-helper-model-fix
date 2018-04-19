d7b5a58d53e55e4d786bb6976635a39d808139fd
Sernin van de Krol <sernin@atane.nl>
2018-04-19 10:07:40 +0200

    Use afterResolving, as contextual binding seems broken

--- src/FixModelServiceProvider.php
+++ src/FixModelServiceProvider.php
@@ -2,20 +2,26 @@
 
 namespace Paneidos\LaravelIdeHelper;
 
+use Barryvdh\LaravelIdeHelper\Console\EloquentCommand;
+use Barryvdh\LaravelIdeHelper\Console\GeneratorCommand;
 use Illuminate\Support\ServiceProvider;
 
 class FixModelServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->app->when(\Barryvdh\LaravelIdeHelper\Console\EloquentCommand::class)
-            ->needs(\Illuminate\Filesystem\Filesystem::class)
-            ->give(ReadOnlyFilesystem::class);
-
-        $this->app->when(\Barryvdh\LaravelIdeHelper\Console\GeneratorCommand::class)
-            ->needs(\Illuminate\Filesystem\Filesystem::class)
-            ->give(function () {
-                $this->app->make(ReadOnlyFilesystem::class, ['only' => base_path('vendor/*')]);
-            });
+    public function register() {
+        $this->app->afterResolving(EloquentCommand::class, function ($resolved, $app) {
+            /** @var EloquentCommand $resolved */
+            $reflection = new \ReflectionClass($resolved);
+            $property = $reflection->getProperty('files');
+            $property->setAccessible(true);
+            $property->setValue($resolved, $app->make(ReadOnlyFilesystem::class));
+        });
+        $this->app->afterResolving(GeneratorCommand::class, function ($resolved, $app) {
+            /** @var GeneratorCommand $resolved */
+            $reflection = new \ReflectionClass($resolved);
+            $property = $reflection->getProperty('files');
+            $property->setAccessible(true);
+            $property->setValue($resolved, $app->make(ReadOnlyFilesystem::class, ['only' => base_path('vendor/*')]));
+        });
     }
 }
